1230d72f943ffe9cfba32351349d2cdfd256edf7
Nguyen Nhan Vu <n.v.nguyen@lancaster.ac.uk>
2022-03-15 21:57:29 +0000

    modify the layout of the game, create a decent game panel with but not yet completed

--- gameBoard.java
+++ gameBoard.java
@@ -4,40 +4,75 @@ import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.*;
 
-
+//drawing the board
 public class gameBoard{
     JFrame main = new JFrame("CodeBreaker");
 
     JPanel main_panel = new JPanel();
+    JPanel empty_panel[] = new JPanel[6];
+    JPanel check_panel[] = new JPanel[6];
+    JPanel input_panel = new JPanel();
 
-    JPanel back_panel = new JPanel();
-
-    JPanel empty_panel = new JPanel();
-
-    JPanel check_panel = new JPanel(); 
-
-    Picture p = new Picture("Empty.png");
-    JLabel l = new JLabel(p);
-
+    JButton input_buttons[] = new JButton[7];
+    
     Icon empty = new ImageIcon("Empty.png");
 
-    JButton b = new JButton(empty);
+    Icon colour_list[] = {new ImageIcon("Colour_0.png"),new ImageIcon("Colour_1.png"),new ImageIcon("Colour_2.png"),new ImageIcon("Colour_3.png"),new ImageIcon("Colour_4.png"),new ImageIcon("Colour_5.png"),new ImageIcon("Colour_6.png")};
+
+    Icon score_0 = new ImageIcon("Score_0.png");
+    Icon score_1 = new ImageIcon("Score_1.png");
+
+    //JButton b = new JButton();
 
     public gameBoard(){
-        main_panel.setLayout(new BoxLayout(main_panel,));
-        back_panel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        empty_panel.setLayout(new GridLayout(1,4));
-        check_panel.setLayout(new GridLayout(2,2));
-        
+        main_panel.setLayout(new GridBagLayout());
+        main_panel.setBackground(Color.DARK_GRAY);
 
+        GridBagConstraints gbc_main = new GridBagConstraints();
+        gbc_main.anchor = GridBagConstraints.NORTH;
+        //gbc_main.fill = GridBagConstraints.VERTICAL;
+        
+        GridBagConstraints gbc_empty = new GridBagConstraints();
+        gbc_empty.weightx = 1;
+        gbc_empty.weighty = 1;
+
+        GridBagConstraints gbc_check = new GridBagConstraints();
+        gbc_check.fill = GridBagConstraints.VERTICAL;
+
+        for(int i = 0; i < 6;i++ ){
+            gbc_empty.gridy = i;
+
+            empty_panel[i] = new JPanel();
+            empty_panel[i].setLayout(new GridBagLayout());
+            check_panel[i] = new JPanel();
+            check_panel[i].setLayout(new GridLayout(2,2));
+            check_panel[i].setPreferredSize(new Dimension(110,110));
+            
+
+            for(int x = 0; x< 4; x++){
+                gbc_empty.gridx = x;
+
+                empty_panel[i].add(new JLabel(empty),gbc_empty);
+                check_panel[i].add(new JLabel(empty));
+
+            }
+            gbc_main.gridy = i;
+            
+            main_panel.add(empty_panel[i],gbc_main);
+            main_panel.add(check_panel[i],gbc_main);
+        }
+
+        for(int i =0; i< colour_list.length ;i++){
+            input_panel.add(new JButton(colour_list[i])); 
+        }
+
+        main_panel.add(input_panel);
 
         //panel.setOpaque(false);
-        main.setContentPane(panel);
-        main.setSize(600,600);
+        main.setContentPane(main_panel);
+        main.setSize(500,900);
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main.setVisible(true);
-
-        main.getContentPane().setBackground(Color.DARK_GRAY);
         
    }
 }
