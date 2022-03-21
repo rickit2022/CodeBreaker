c84fa33196670d7b1a0347a436a2ee9467c4af26
Nguyen Nhan Vu <n.v.nguyen@lancaster.ac.uk>
2022-03-21 06:51:18 +0000

    Tidy up the driver.

--- gameBoardDriver.java
+++ gameBoardDriver.java
@@ -1,10 +1,5 @@
-import javax.swing.*;
-import javax.swing.plaf.ColorUIResource;
-import java.awt.*;
-import java.awt.event.*;
-
 public class gameBoardDriver{
     public static void main(String[] args){
-        gameBoard frame1 = new gameBoard();
+            new gameBoard();
     }
 }
